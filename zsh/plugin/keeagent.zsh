6577677bc91bd5309386d34c0b9bc99a1c80584b
shiro <shiro@usagi.io>
2017-10-27 14:35:03 +0200

    Fixed keeagent socket

--- zsh/plugin/keeagent.zsh
+++ zsh/plugin/keeagent.zsh
@@ -1,6 +1,8 @@
-if [ -z $SSH_AUTH_SOCK ]; then
+if [[ -z $SSH_AUTH_SOCK ]]; then
 	export SSH_AUTH_SOCK="/tmp/.ssh-auth-sock"
-	if [ ! -S  "$SSH_AUTH_SOCK" ]; then
+fi
+
+
+if [[ ! -f /tmp/msysgit2unix-socket.pid ]]; then
 		~/bin/msysgit2unix-socket.py "$(realpath ~/.ssh/keeagent.sock):$SSH_AUTH_SOCK"
-	fi
 fi
