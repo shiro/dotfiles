bbbf469c3256a31d8ff2b6faf51c367509c84d50
shiro <shiro@usagi.io>
2017-11-01 18:51:50 +0100

    Added env variables for WSL

--- zsh/_env.zsh
+++ zsh/_env.zsh
@@ -1,8 +1,6 @@
 export EDITOR=/usr/bin/vim
 export DISPLAY=localhost:0
 
-# Import SSH-Keys from KeePass KeeAgent
-# if [[ -z $SSH_AUTH_SOCK ]]; then
-#	export SSH_AUTH_SOCK="/tmp/.ssh-auth-sock"
-#	python3 ~/bin/msysgit2unix-socket.py ~/.ssh/keeagent.sock:$SSH_AUTH_SOCK
-# fi
+# win path
+export PATH="$PATH:/mnt/c/Windows/System32"
+
