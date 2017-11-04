6a1b73aa71599d8545af229b3af9b0e0d7b283db
shiro <shiro@usagi.io>
2017-11-05 00:42:28 +0100

    Added reloading of keepass socket

--- zsh/plugin/keeagent.zsh
+++ zsh/plugin/keeagent.zsh
@@ -2,7 +2,12 @@ if [[ -z $SSH_AUTH_SOCK ]]; then
 	export SSH_AUTH_SOCK="/tmp/.ssh-auth-sock"
 fi
 
+keepass(){
+	if [ -f /tmp/msysgit2unix-socket.pid ]; then
+		rm /tmp/msysgit2unix-socket.pid $SSH_AUTH_SOCK 2>/dev/null
+	fi
+	python2 ~/bin/msysgit2unix-socket.py "$(realpath ~/.ssh/keeagent.sock):$SSH_AUTH_SOCK"
+}
 
-if [[ ! -f /tmp/msysgit2unix-socket.pid ]]; then
-		python2 ~/bin/msysgit2unix-socket.py "$(realpath ~/.ssh/keeagent.sock):$SSH_AUTH_SOCK"
-fi
+# load keepass socket on startup if it's not there
+[ ! -f /tmp/msysgit2unix-socket.pid ] && keepass
