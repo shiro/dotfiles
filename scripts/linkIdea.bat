652a1bdadc6f797b7bd2477138f8f40325bcb139
shiro <shiro@usagi.io>
2018-03-19 20:08:12 +0100

    Add batch script to link IDEA directories

---
+++ scripts/linkIdea.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set home=%userprofile%
+
+set fileList=("%home%\.PhpStorm*" ^
+              "%home%\.IntelliJIdea*" ^
+              "%home%\.Rider*" ^
+              "%home%\.GoLand*" ^
+              "%home%\.DataGrip*" ^
+              "%home%\.CLion*" ^
+             )
+
+echo searching for IDEA applications in %home%
+echo =========================================
+
+for /d %%F in %fileList% do (
+  echo [LINK] %%F
+
+  for %%x in (keymaps colors codestyles) do (
+    rd /s /q "%%F\config\%%x" > nul 2>&1
+    mklink /J "%%F\config\%%x" ".\\idea\\%%x" > nil
+  )
+)
+
+echo =========================================
+echo linking finished
