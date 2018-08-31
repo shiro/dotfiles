eb471fac5a65b8aef8d7adf351b1c7cebca2bd36
shiro <shiro@usagi.io>
2018-08-31 13:34:05 +0200

    Fix typo

--- config/zsh/plugins/tmux-startup.zsh
+++ config/zsh/plugins/tmux-startup.zsh
@@ -1,5 +1,5 @@
 [[ -z "$TMUX" && -n "$TMUX_ID" ]] && {
-	local SESSION="sesstion_$TMUX_ID"
+	local SESSION="session_$TMUX_ID"
 
 	tmux a -t "$SESSION" 2>/dev/null || {
 		cd && exec tmux new -s "$SESSION"
