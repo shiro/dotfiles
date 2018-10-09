72ead4797d87c3595ce1707f68e6f96b120c9177
shiro <shiro@usagi.io>
2018-10-09 15:20:11 +0200

    Improve intelliJ linking

--- scripts/windows/link-intellij.bat
+++ scripts/windows/link-intellij.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set home=%userprofile%
+set dotIdeaDir=%~dp0..\..\idea
 
 set fileList=("%home%\.PhpStorm*" ^
               "%home%\.IntelliJIdea*" ^
@@ -16,9 +17,16 @@ echo =========================================
 for /d %%F in %fileList% do (
   echo [LINK] %%F
 
+  rem link folders
   for %%x in (keymaps colors codestyles) do (
     rd /s /q "%%F\config\%%x" > NUL 2>&1
-    mklink /J "%%F\config\%%x" "%~dp0\\..\\..\\idea\\%%x" > NUL
+    mklink /J "%%F\config\%%x" "%dotIdeaDir%\%%x" > NUL
+  )
+
+  rem link options
+  for /f %%i in ('dir /b "%dotIdeaDir%\options\*"') do (
+    rm %%F\config\options\%%i > NUL 2>&1
+    mklink "%%F\config\options\%%i" "%dotIdeaDir%\options\%%i" > NUL
   )
 )
 
