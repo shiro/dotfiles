54e4eeed9aac7d51de2b255190f3eb88848de10c
shiro <shiro@usagi.io>
2019-10-05 21:28:29 +0200

    Update keepass plugin to support WSL2

--- config/zsh/plugins/keeagent.zsh
+++ config/zsh/plugins/keeagent.zsh
@@ -1,10 +1,6 @@
-if [[ -z $SSH_AUTH_SOCK ]]; then
-	export SSH_AUTH_SOCK="/tmp/.ssh-auth-sock"
-fi
+WEASELPAGENTDIR=/mnt/c/etc/weasel-pageant
 
-keepass(){
-	if [ -f /tmp/msysgit2unix-socket.pid ]; then
-		rm /tmp/msysgit2unix-socket.pid $SSH_AUTH_SOCK 2>/dev/null
-	fi
-	python2 ~/bin/msysgit2unix-socket "$(realpath ~/.ssh/keeagent.sock):$SSH_AUTH_SOCK"
-}
+# killing old running socket
+#$WEASELPAGENTDIR/weasel-pageant -k >/dev/null 2>/dev/null
+
+eval $($WEASELPAGENTDIR/weasel-pageant -rba "/tmp/.weasel-pageant-$USER") >/dev/null 2>/dev/null
