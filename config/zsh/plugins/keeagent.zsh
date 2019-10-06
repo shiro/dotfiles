1ae80feb1aa2451e885ee1d12298e1d2c17e7156
shiro <shiro@usagi.io>
2019-10-06 13:12:32 +0200

    Update keepass script

--- config/zsh/plugins/keeagent.zsh
+++ config/zsh/plugins/keeagent.zsh
@@ -1,6 +1,15 @@
 WEASELPAGENTDIR=/mnt/c/etc/weasel-pageant
 
-# killing old running socket
-#$WEASELPAGENTDIR/weasel-pageant -k >/dev/null 2>/dev/null
+local keepass-init(){
+  eval $($WEASELPAGENTDIR/weasel-pageant -rba "/tmp/.weasel-pageant-$USER") >/dev/null 2>/dev/null
+}
 
-eval $($WEASELPAGENTDIR/weasel-pageant -rba "/tmp/.weasel-pageant-$USER") >/dev/null 2>/dev/null
+# killing old running socket
+keepass(){
+  $WEASELPAGENTDIR/weasel-pageant -k >/dev/null 2>/dev/null
+  rm "/tmp/.weasel-pageant-$USER" 2>/dev/null
+
+  keepass-init
+}
+
+keepass-init
