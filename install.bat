7a96e088451cc2295f370c862b89da7017df1029
shiro <shiro@usagi.io>
2019-10-08 20:28:33 +0200

    Add windows install file

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+
+net session >nul 2>&1
+if NOT %errorLevel% == 0 (
+    echo "error: administrator permissions are required"
+    exit 1
+)
+
+set home=%userprofile%
+set dotfiles=%~dp0
+
+echo "Installing dotfiles."
+
+mkdir %home%\.config 2>&1
+
+mklink /D %home%\.config\mintty %dotfiles%\config\mintty
+
+echo "Done. Reload your terminal."
