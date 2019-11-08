f9d980f146b1ed9c829086ccf18ee3723608a119
shiro <shiro@usagi.io>
2019-11-09 00:07:16 +0100

    Move diary and wiki to scripts

--- config/zsh/alias/workflow.zsh
+++ config/zsh/alias/workflow.zsh
@@ -6,6 +6,3 @@ compdef '_files -W ~/project/work/ -g "~/project/work/*"' work
 
 alias dt='cd ~/Desktop'
 alias dot="cd $DOTFILES"
-
-alias diary="vim -c 'VimwikiMakeDiaryNote'"
-alias wiki="vim -c 'VimwikiIndex'"
