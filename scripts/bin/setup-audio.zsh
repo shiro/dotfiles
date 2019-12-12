96009aabe5efeb3c3717af4f1d7b75258434f19f
shiro <shiro@usagi.io>
2019-12-12 18:05:21 +0100

    Add setup audio script

---
+++ scripts/bin/setup-audio.zsh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+killall cadence carla non-mixer
+
+cadence-session-start -s
+
+(
+  # mixer
+  non-mixer ~/delme/mixer &
+
+  sleep 5;
+
+  # plugin host
+  carla ~/delme/example1.carxp &
+) &
+
+# communication
+pactl load-module module-jack-sink   client_name=comm-out connect=no
+pactl load-module module-jack-source client_name=comm-in  connect=no
+
+# browser
+pactl load-module module-jack-sink   client_name=browser_out connect=no
+
+# foobar
+pactl load-module module-jack-sink   client_name=foobar_out connect=no
+
+# general use busses
+pactl load-module module-jack-sink   client_name=gen1_out connect=no
+pactl load-module module-jack-sink   client_name=gen2_out connect=no
+pactl load-module module-jack-sink   client_name=gen3_out connect=no
