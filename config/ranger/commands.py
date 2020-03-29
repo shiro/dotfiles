1277baa98db42141771749b4521f8213661fed30
shiro <shiro@usagi.io>
2020-03-29 12:28:39 +0200

    Add ranger config

---
+++ config/ranger/commands.py
@@ -0,0 +1,59 @@
+# This is a sample commands.py.  You can add your own commands here.
+#
+# Please refer to commands_full.py for all the default commands and a complete
+# documentation.  Do NOT add them all here, or you may end up with defunct
+# commands when upgrading ranger.
+
+# A simple command for demonstration purposes follows.
+# -----------------------------------------------------------------------------
+
+from __future__ import (absolute_import, division, print_function)
+
+# You can import any python module as needed.
+import os
+
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
+
+class fzf_select(Command):
+    """
+    :fzf_select
+    Find a file using fzf.
+    With a prefix argument select only directories.
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+class autojump_fzf_cd(Command):
+    """
+    autojump fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+
+        command="cat ~/.local/share/autojump/autojump.txt | sort -nr | awk -F '\t' '{print $NF}' | fzf +s"
+
+        process = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            target_dir = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.cd(target_dir)
---
+++ config/ranger/plugins/autojump.py
@@ -0,0 +1,35 @@
+import os
+import ranger.api
+import subprocess
+from ranger.api.commands import *
+
+HOOK_INIT_OLD = ranger.api.hook_init
+
+
+def hook_init(fm):
+    def update_autojump(signal):
+        subprocess.Popen(["autojump", "--add", signal.new.path])
+
+    def update_autojump_file(signal):
+        os.environ["IGNORE_INITIAL_AUTOJUMP"] = "1"
+        subprocess.Popen(["autojump", "--add", fm.thisfile.path])
+
+    fm.signal_bind('cd', update_autojump)
+    fm.signal_bind('execute.before', update_autojump_file)
+    HOOK_INIT_OLD(fm)
+
+
+ranger.api.hook_init = hook_init
+
+
+class j(Command):
+    """:j
+
+    Uses autojump to set the current directory.
+    """
+
+    def execute(self):
+        directory = subprocess.check_output(["autojump", self.arg(1)])
+        directory = directory.decode("utf-8", "ignore")
+        directory = directory.rstrip('\n')
+        self.fm.execute_console("cd " + directory)
