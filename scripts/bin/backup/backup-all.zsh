f3b88373bf24054204a14bdf0aade9bb7c165a67
shiro <shiro@usagi.io>
2020-04-06 22:58:12 +0200

    backup-all attempt to key passphrase from keyring

--- scripts/bin/backup/backup-all.zsh
+++ scripts/bin/backup/backup-all.zsh
@@ -3,6 +3,13 @@
 BACKUP_LOCATION="/mnt/hdd1/backup/`hostname`"
 
 
+# attempt to read passphrase from the keyring
+env_passphrase="$(secret-tool lookup Title "system-backup-reference")"
+if [ -n "$env_passphrase" ];then
+  export BORG_PASSPHRASE="$env_passphrase"
+fi
+
+
 # read the passphrase
 if [ -z "$BORG_PASSPHRASE" ]; then
   echo -n 'Repo passphrase:'
