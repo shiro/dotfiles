400617b3ce274d3ef3dd5891c26442d9c173210c
shiro <shiro@usagi.io>
2020-09-03 13:26:24 +0200

    Add styled components snippet

---
+++ config/snip/react/styled-component/styled-component.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import {styled} from "linaria/react";
+import {bodyText, primaryFont} from "style/commonStyle";
+
+
+interface IProps { }
+
+
+const {{ name }}: React.FC<IProps> = (props) => {
+    return (
+        <Container>
+          
+        </Container>
+    );
+};
+
+
+export default {{ name }};
+
+
+const Container = styled.div`
+  ${bodyText}
+  ${primaryFont}
+`;
