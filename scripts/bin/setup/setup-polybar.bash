e86c8087a6cc3b7fdffc9d29f1c0b26dae4a18f3
shiro <shiro@usagi.io>
2020-09-09 10:55:05 +0200

    Update polybar monitor detection regex

--- scripts/bin/setup/setup-polybar.bash
+++ scripts/bin/setup/setup-polybar.bash
@@ -2,4 +2,6 @@
 
 killall polybar 2>/dev/null
 
-xrandr | grep -oP '((eDP|DisplayPort)[^\s]*)' | xargs -P1 -I{} bash -c 'MONITOR={} polybar main &'
+# xrandr | grep -oP '((eDP|DisplayPort|HDMI)[^\s]*connected[^\s]*)' 
+
+xrandr | grep -oP '(eDP|DisplayPort|HDMI)[^\s]*(?= +connected)'| xargs -P1 -I{} bash -c 'MONITOR={} polybar main &'
