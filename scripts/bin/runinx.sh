1913f4ea7729bc8b99fba748fe206c66da965e78
shiro <shiro@usagi.io>
2021-08-26 20:33:18 +0200

    Update runinx

--- scripts/bin/runinx.sh
+++ scripts/bin/runinx.sh
@@ -2,4 +2,4 @@
 
 USERID=1000
 
-/bin/su shiro -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USERID/bus DISPLAY=:0 $1"
+/bin/su shiro -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USERID/bus DISPLAY=:0 $*"
