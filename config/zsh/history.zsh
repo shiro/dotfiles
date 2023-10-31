f6fbd764e027300a0d582159c4a134aeca49e552
shiro <shiro@usagi.io>
2023-11-01 08:33:34 +0900

    Add history flag

--- config/zsh/history.zsh
+++ config/zsh/history.zsh
@@ -18,6 +18,7 @@ esac
 setopt EXTENDED_HISTORY
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS # ignore duplication command history list
+setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_VERIFY
 # setopt INC_APPEND_HISTORY
