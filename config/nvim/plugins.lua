6d86f4b256b8868c0ac825dd9fad07fe0aff85aa
shiro <shiro@usagi.io>
2023-12-10 20:10:20 +0900

    Update vim config

--- config/nvim/plugins.lua
+++ config/nvim/plugins.lua
@@ -1,8 +1,9 @@
-local utils = require "utils"
+-- local utils = require "utils"
 
 local call = vim.call
 local cmd = vim.cmd
 local Plug = vim.fn['plug#']
+
 --local PATH = "~/.config/nvim/plugged"
 --call('plug#begin', PATH)
 
@@ -17,18 +18,18 @@ Plug 'nvim-treesitter/nvim-treesitter'
 
 -- LSP server, auto-complete
 vim.g['coc_global_extensions'] = {
-  'coc-json',
-  'coc-vimlsp',
-  'coc-rust-analyzer',
-  'coc-tsserver',
-  'coc-styled-components',
-  'coc-prettier',
-  'coc-eslint',
-  -- 'coc-lua',
-  'coc-sumneko-lua',
-  'coc-pyright',
+    'coc-json',
+    'coc-vimlsp',
+    'coc-rust-analyzer',
+    'coc-tsserver',
+    'coc-styled-components',
+    'coc-prettier',
+    'coc-eslint',
+    -- 'coc-lua',
+    'coc-sumneko-lua',
+    'coc-pyright',
 }
-Plug('neoclide/coc.nvim', {branch = 'master', ['do'] = 'npm ci'})
+Plug('neoclide/coc.nvim', { branch = 'master', ['do'] = 'npm ci' })
 Plug 'antoinemadec/coc-fzf'
 
 -- Plug 'ray-x/guihua.lua'
@@ -37,8 +38,8 @@ Plug 'antoinemadec/coc-fzf'
 -- language-specific stuff {{{
 
 -- rust
-Plug('rust-lang/rust.vim', {['for'] = 'rust'})
-Plug('arzg/vim-rust-syntax-ext', {['for'] = 'rust'})
+Plug('rust-lang/rust.vim', { ['for'] = 'rust' })
+Plug('arzg/vim-rust-syntax-ext', { ['for'] = 'rust' })
 
 -- }}}
 
@@ -62,17 +63,25 @@ Plug 'norcalli/nvim-colorizer.lua'
 --- }}}
 
 Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', {tag = '0.1.5'})
+Plug('nvim-telescope/telescope.nvim', { tag = '0.1.5' })
+Plug 'fannheyward/telescope-coc.nvim'
 
 call 'plug#end'
 
-require('telescope').setup{
+require('telescope').setup {
     --defaults = { mappings = { i = { ["<esc>"] = telescope_actions.close } } },
     file_ignore_patterns = {
         "node%_modules/.*",
         "./target/.*",
+    },
+    extensions = {
+        coc = {
+            theme = 'ivy',
+            prefer_locations = true, -- use for list picker
+        }
     }
 }
+local sorter = require("top-results-sorter").sorter()
 local tele_builtin = require('telescope.builtin')
 local tele_theme_dropdown = require "telescope.themes".get_dropdown()
 local tele_theme_cursor = require "telescope.themes".get_cursor()
@@ -87,17 +96,19 @@ function dynamic_theme()
         return tele_theme_dropdown
     end
 end
+
 local theme = dynamic_theme
 --require("dressing").setup { select = { telescope = tele_theme_cursor } }
 --vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files({sort_lastused= 1})<CR>', {})
 function _G.foo()
-        tele_builtin.find_files( {
-            sorter = require("top-results-sorter").sorter(),
-            previewer = false,
-            --find_command = { "bash", "-c",
-            --    "PATH=$PATH:~/.cargo/bin rg --files --one-file-system --color never --sort modified" }
-        })
+    tele_builtin.find_files({
+        sorter = sorter,
+        previewer = false,
+        --find_command = { "bash", "-c",
+        --    "PATH=$PATH:~/.cargo/bin rg --files --one-file-system --color never --sort modified" }
+    })
 end
+
 vim.keymap.set("n", "<leader>ff", '<CMD>lua _G.foo()<CR>', {})
 
 --vim.api.nvim_set_keymap('n', "<leader>ff",
@@ -124,16 +135,16 @@ vim.keymap.set("n", "<leader>ff", '<CMD>lua _G.foo()<CR>', {})
 --  cmds_list = {}
 --})
 
-require'colorizer'.setup()
+require 'colorizer'.setup()
 
 -- tree-sitter {{{
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"typescript", "tsx", "javascript", "css", "scss", "rust", "json", "lua" },
-  auto_install = true,
-  highlight = { enable = true },
-  incremental_selection = { enable = true },
-  indent = { enable = true },
-  context_commentstring = { enable = true },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "typescript", "tsx", "javascript", "css", "scss", "rust", "json", "lua" },
+    auto_install = true,
+    highlight = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = true },
+    context_commentstring = { enable = true },
 }
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
@@ -141,23 +152,30 @@ vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 
 
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+
+
 vim.api.nvim_set_keymap('n', 'gd', "<Plug>(coc-definition)", { silent = true });
 vim.api.nvim_set_keymap('n', 'gy', "<Plug>(coc-type-definition)", { silent = true });
 vim.api.nvim_set_keymap('n', 'gi', "<Plug>(coc-implementation)", { silent = true });
-vim.api.nvim_set_keymap('n', 'gr', "<Plug>(coc-references)", { silent = true });
+vim.api.nvim_set_keymap('n', 'gr', ":Telescope coc references<CR>", { silent = true });
 vim.api.nvim_set_keymap('n', ']e', "<Plug>(coc-diagnostic-next)", { silent = true });
 vim.api.nvim_set_keymap('n', '[e', "<Plug>(coc-diagnostic-prev)", { silent = true });
 vim.api.nvim_set_keymap('n', '<A-S-e>', "<Plug>(coc-rename)", { silent = true });
 vim.api.nvim_set_keymap('n', '<A-S-r>', "<Plug>(coc-refactor)", { silent = true });
 vim.api.nvim_set_keymap('v', '<A-S-r>', "<Plug>(coc-refactor-selected)", { silent = true });
+-- TODO go to symbol
+-- :Telescope coc workspace_symbols
+
 -- show outline (hierarchy)
 vim.api.nvim_set_keymap('n', 'go', ":CocFzfList outline<CR>", { silent = true });
+-- vim.api.nvim_set_keymap("n", "go", ":CocList outline<CR>", opts)
 -- list warnings/errors
-vim.api.nvim_set_keymap('n', 'ge', ":CocFzfList diagnostics<CR>", { silent = true });
+-- vim.api.nvim_set_keymap('n', 'ge', ":CocFzfList diagnostics<CR>", { silent = true });
 -- list all local changes
 vim.api.nvim_set_keymap('n', 'gD', ":GF?<CR>", { silent = true });
 
-vim.keymap.set('i', '<C-Space>', 'coc#refresh()', { expr = true, silent = true})
+vim.keymap.set('i', '<C-Space>', 'coc#refresh()', { expr = true, silent = true })
 
 -- highlight the symbol under cursor
 vim.api.nvim_create_augroup("CocGroup", {})
@@ -186,22 +204,23 @@ function _G.show_docs()
         vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
     end
 end
-vim.keymap.set("n", "<C-S-p>", '<CMD>lua _G.show_docs()<CR>', {noremap = true, silent = true})
-vim.keymap.set("n", "<F12>", '<CMD>lua _G.show_docs()<CR>', {noremap = true, silent = true})
+
+vim.keymap.set("n", "<C-S-p>", '<CMD>lua _G.show_docs()<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<F12>", '<CMD>lua _G.show_docs()<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-S-p>', "CocActionAsync('showSignatureHelp')", { silent = true, expr = true });
 vim.keymap.set('i', '<F12>', "CocActionAsync('showSignatureHelp')", { silent = true, expr = true });
 
 
-vim.keymap.set("n", "<A-S-i>", ':Files<CR>', {noremap = true, silent = true})
-
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+vim.keymap.set("n", "<A-S-i>", ':Files<CR>', { noremap = true, silent = true })
 
 -- tab/S-tab completion menu
 function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
-vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+
+vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
+    opts)
 vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 -- code lens
 vim.keymap.set("n", "ga", "<Plug>(coc-codelens-action)", opts)
@@ -209,27 +228,43 @@ vim.keymap.set("n", "ga", "<Plug>(coc-codelens-action)", opts)
 vim.keymap.set("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 -- reformat code
 function _G.format()
-  if vim.bo.filetype == 'rust' then
-     vim.cmd('RustFmt')
-  elseif vim.bo.filetype == 'typescriptreact' then
-    vim.cmd('CocCommand prettier.forceFormatDocument')
-  end
+    if vim.bo.filetype == 'rust' then
+        vim.cmd('RustFmt')
+    elseif vim.bo.filetype == 'typescriptreact' then
+        vim.cmd('CocCommand prettier.forceFormatDocument')
+    end
 end
-vim.keymap.set('n', 'gl', '<CMD>lua _G.format()<CR>', {silent = true})
+
+-- vim.keymap.set('n', 'gl', '<CMD>lua _G.format()<CR>', {silent = true})
+vim.keymap.set("x", "gl", "<Plug>(coc-format-selected)", { silent = true })
+vim.keymap.set("n", "gl", "<Plug>(coc-format-selected)j", { silent = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = "CocGroup",
+    pattern = "lua",
+    command = "setl formatexpr=CocAction('formatSelected')",
+    desc = "Setup formatexpr specified filetype(s)."
+})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     group = "CocGroup",
+--     pattern = "rust",
+--     command = "setl formatexpr=RustFmt",
+--     desc = "Setup formatexpr specified filetype(s)."
+-- })
 
 
 -- auto-format on save
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = {'*.rs', '*.tsx'},
-  callback = _G.format
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = {'*.rs', '*.tsx'},
+--   callback = _G.format
+-- })
 
 -- auto-save on focus lost/buffer change
 vim.o.autowriteall = true
 vim.api.nvim_create_autocmd("FocusLost", {
-  pattern = '*.*',
-  callback = function()
-    _G.format()
-    vim.cmd.write({mods = {silent = true}})
-  end
+    pattern = '*.*',
+    callback = function()
+        _G.format()
+        vim.cmd.write({ mods = { silent = true } })
+    end
 })
