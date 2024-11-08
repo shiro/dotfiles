d10b860e6631c962dd887b11e21dea37fa5fb8bd
shiro <shiro@usagi.io>
2024-11-09 07:49:53 +0900

    Update script

--- config/zsh/alias/work.zsh
+++ config/zsh/alias/work.zsh
@@ -1,10 +1,9 @@
 dmm-branch-up () {
-  if [ $1 ]
-  then
-    branchctl server-replace create $1 --frontend $1
+  if [ $# -eq 1 ]; then
+    branchctl server-replace create $1 --frontend $@
   else
     local branchname
     branchname=$(git branch --show-current) 
-    branchctl server-replace create $branchname --frontend $branchname
+    branchctl server-replace create $branchname --frontend $branchname $@
   fi
 }
