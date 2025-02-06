4c3edc8f2e1be06a02c89f3ac7f65939cc7852a9
shiro <shiro@usagi.io>
2025-02-06 14:26:05 +0000

    Update clipboard script

--- scripts/src/util/clipboard-rotate.zsh
+++ scripts/src/util/clipboard-rotate.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-cliphist list | head -n2 | tail -n1 | cliphist decode | wl-copy
+cliphist list | head -n1 | cliphist delete
+cliphist list | head -n1 | cliphist decode | wl-copy
