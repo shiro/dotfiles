35591ede5cbd8cdefd5165afcbffe81744c66143
shiro <shiro@usagi.io>
2025-03-10 14:42:57 +0900

    Update nix shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,18 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { 
-    nativeBuildInputs = with pkgs; [ rustc cargo gcc upx ];
-
-    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust = pkgs.rust-bin.nightly."2025-03-10".default.override {
+    extensions = [ "rust-src" ];
+  };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ upx ];
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+  ]);
+  RUST_BACKTRACE = 1;
 }
-
