2e09369527aa768a1bb5bfd317af82d2ee3c1722
shiro <shiro@usagi.io>
2025-08-14 15:14:18 +0900

    Update scripts

--- scripts/src/media/screencap.bash
+++ scripts/src/media/screencap.bash
@@ -15,12 +15,13 @@
 # mkdir -p "$(xdg-user-dir VIDEOS)"
 # cd "$(xdg-user-dir VIDEOS)" || exit
 if pgrep wf-recorder > /dev/null; then
-    notify-send "Recording saved" &
+    notify-send "Recording saved" -t 2000 &
     pkill wf-recorder &
 else
     file="$HOME/Videos/screencaps/$(date +'%Y-%m-%d-%H%M%S.mp4')"
     mkdir -p "$(dirname "$file")"
     wf-recorder -g "$(slurp)" --file="$file"
+    wl-copy < "$file"
     # if [[ "$1" == "--sound" ]]; then
     #     wf-recorder --pixel-format yuv420p -f './recording_'"$(getdate)"'.mp4' -t --geometry "$(slurp)" --audio="$(getaudiooutput)" & disown
     # elif [[ "$1" == "--fullscreen-sound" ]]; then
--- scripts/src/workflow/wiki.zsh
+++ scripts/src/workflow/wiki.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 class=`hyprctl activewindow | grep class: | cut -d' ' -f2`
-vim_cmd="vim -c 'cd wiki'"
+vim_cmd="cd wiki && vim"
 
 if [ "$class" = "Alacritty" ]; then
     tmux new-window -a $vim_cmd
