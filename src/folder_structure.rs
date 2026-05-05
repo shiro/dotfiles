fdcdf311001b15f5271c2102a2470fe0a214d5e3
shiro <shiro@usagi.io>
2026-05-05 15:44:03 +0900

    Improve dot setup

--- src/folder_structure.rs
+++ src/folder_structure.rs
@@ -1,33 +1,58 @@
 use crate::*;
+use std::fs::copy;
+use walkdir::WalkDir;
+
+fn process_template_dir() -> Result<()> {
+    let home = std::env::var("HOME")?;
+    let cwd = std::env::current_dir()?;
+    let template_dir_path = Path::new(&cwd).join("template");
+
+    if !template_dir_path.exists() {
+        return Ok(());
+    }
+
+    for entry in WalkDir::new(&template_dir_path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path() != template_dir_path)
+    {
+        let dst_path = Path::new(&home).join(entry.path().strip_prefix(&template_dir_path)?);
+        if !dst_path.exists() {
+            if entry.path().is_dir() {
+                create_dir_all(&dst_path)?;
+            } else if entry.path().is_file() {
+                if let Some(parent) = dst_path.parent() {
+                    create_dir_all(parent)?;
+                }
+                copy(entry.path(), &dst_path)?;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 pub fn main() -> Result<()> {
     let home = std::env::var("HOME")?;
 
     println!("setting up folder structure");
 
-    println!(
-        indoc::indoc! {"
-            relevant folders:
-            |--- {}/
-            | |--- bin/             local script files
-            |
-            |--- .local/            local files
-            | |---> share/          temporary files
-            | |---> config/         local configuration files
-            |   |---> backup/       backup config
-            |   |---> zsh/          zsh config
-            |
-            |--- project/           development projects
-            |--- wallpapers/        wallpapers
-        "},
-        home
-    );
+    create_dir_all(format!("{}/project", home))?;
 
     create_dir_all(format!("{}/wallpapers", home))?;
-    let _ = symlink(
-        format!("{home}/.dotfiles/wallpapers/sample-wallpaper.jpg"),
-        format!("{home}/wallpapers/sample-wallpaper.jpg"),
-    );
+    for entry in std::fs::read_dir("wallpapers")? {
+        let entry = entry?;
+        let path = std::fs::canonicalize(entry.path())?;
+        if let Some(file_name) = path.file_name() {
+            println!("{:?}", (&path, file_name));
+            let _ = symlink(
+                &path,
+                format!("{home}/wallpapers/{}", file_name.to_string_lossy()),
+            );
+        }
+    }
+
+    process_template_dir()?;
 
     Ok(())
 }
